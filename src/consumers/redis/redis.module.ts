2693fa40d475d5e691b2df30717c631d29a434fd
Fedorichev Lev Aleksandrovich <fedorichev16@gmail.com>
2025-02-12 02:40:40 +0300

    start redis part

--- src/consumers/redis/redis.module.ts
+++ src/consumers/redis/redis.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { ClientsModule } from '@nestjs/microservices';
+import options from '~src/consumers/redis/options';
 import { ConfigModule } from '~src/config/config.module';
 import { LoggerModule } from '~src/logger/logger.module';
 import { EventsModule } from '~src/events/events.module';
 
 @Module({
-  imports: [ConfigModule, LoggerModule, EventsModule],
+  imports: [ClientsModule.registerAsync([options]), ConfigModule, LoggerModule, EventsModule],
   providers: [RedisService],
+  exports: [RedisService],
 })
 export class RedisModule {}
